a213ce77402157d3c7d83ce1cf6e55c3672f29c7
yowaikami <hinzyakushi@gmail.com>
2020-04-23 20:39:06 +0900

    Java 変数関係基礎

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String []args) {
+		String name = "Sakamoto";
+		System.out.println(name);
+		name = "四面楚歌";
+		System.out.println(name);
+		final int Lucky = 7;
+		System.out.println(Lucky);
+
+	}
+
+}
---
+++ JavaWcp/src/Variable02.java
@@ -0,0 +1,7 @@
+
+public class Variable02 {
+	public static void main(String[] args) {
+		final int MIN_NUMBER = 10;
+		System.out.println(MIN_NUMBER);
+	}
+}
