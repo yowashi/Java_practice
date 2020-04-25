c0ea2bc8ac1a8fc2f9ab62882892b71e9434afd1
yowaikami <hinzyakushi@gmail.com>
2020-04-25 18:24:28 +0900

    データ型学習

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1234L;
+		double double1 = 2.3;
+		boolean bol1 = true;
+
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(bol1);
+
+		Float flo1 = 2.3f;
+		Integer int1 = 12;
+		Character cha1 = 'あ';
+
+		System.out.println(flo1);
+		System.out.println(int1);
+		System.out.println(cha1);
+
+		int[] array1 = { 1, 2, 3, 4, 5 };
+
+		System.out.println(array1[3]);
+	}
+
+}
