116dc9e1da2d062fbfa6458b42334a820a787823
yowaikami <hinzyakushi@gmail.com>
2020-05-01 01:53:35 +0900

    例外処理　途中

--- JavaWcp/src/classMethod/Chapter09.java
+++ JavaWcp/src/classMethod/Chapter09.java
@@ -1,5 +1,17 @@
 package classMethod;
 
-public class Chapter09 {
+import classMethod.human.Car;
 
-}
+public class Chapter09 {
+	    public static void main(String[] args) {
+	        // 燃費（Km/L）
+	        double fuelCost = 12.5;
+	        // 残量（L）
+	        double fuelAmount = 55.0;
+
+	        Car car = new Car(fuelCost, fuelAmount);
+
+	        car.move(20);
+	        System.out.println("残量は、" + car.getFuelAmount() + "です");
+	    }
+	}
