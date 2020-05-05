66a8ef271eead49eb8c42467eb4fd4838e47f4fe
yowaikami <hinzyakushi@gmail.com>
2020-05-05 23:29:27 +0900

    カプセル化　終了

--- JavaWcp/src/capsule/Capsule01.java
+++ JavaWcp/src/capsule/Capsule01.java
@@ -1,11 +1,11 @@
 package capsule;
 
 import capsule.human.HumanCapsule;
-import capsule.human.HumanNoCapsule;
+import capsule.human.CapsuleMan;
 
 public class Capsule01 {
 	public static void main(String[] args) {
-		HumanNoCapsule human1 = new HumanNoCapsule("田中",28);
+		CapsuleMan human1 = new CapsuleMan("田中",28);
 		human1.name = "今田";
 		human1.age = 70;
 		System.out.println("名前は" + human1.name + "、年齢は" +human1.age + "です");
---
+++ JavaWcp/src/capsule/human/CapsuleMan.java
@@ -0,0 +1,23 @@
+package capsule.human;
+
+public class CapsuleMan {
+	private String name;
+	private int age;
+
+	public CapsuleMan(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
