0572b95f78c12c86fde1a8c9d6dc74d45cbf88fb
yowaikami <hinzyakushi@gmail.com>
2020-05-13 22:01:09 +0900

    途中経過

--- JavaWcp/src/practice/Chapter15.java
+++ JavaWcp/src/practice/Chapter15.java
@@ -1,5 +1,16 @@
 package practice;
 
 public class Chapter15 {
+	public static void main(String[] args) {
+		int a = 976;
+		int b = 427;
+		System.out.println(a + "と" + b + "の最大公約数" + gcb(a, b) + "です");
+	}
 
+	static int gcb(int a, int b) {
+		if ( b == 0) {
+			return a;
+		}
+		return gcb(b, a % b);
+	}
 }
