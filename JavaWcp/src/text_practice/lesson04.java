f0ad8b199cb70fe25f9373065586f7b764c96173
yowaikami <hinzyakushi@gmail.com>
2020-05-15 01:24:50 +0900

    オブジェクトの使い方完

--- JavaWcp/src/text_practice/lesson04.java
+++ JavaWcp/src/text_practice/lesson04.java
@@ -1,5 +1,20 @@
 package text_practice;
 
 public class lesson04 {
+	public static void main(String[] args) {
+		int x, y;
+		x = 10;
+		y = 3;
 
+		java.awt.Point point = new java.awt.Point();
+		point.setLocation(x, y);
+
+		System.out.println("x, y座標をフィールドで参照します(int)");
+		System.out.println("x座標:" + point.x);
+		System.out.println("y座標:" + point.y);
+
+		System.out.println("x, y座標をgetメソッドで参照します(double)");
+		System.out.println("x座標:" + point.getX());
+		System.out.println("y座標:" + point.getY());
+	}
 }
