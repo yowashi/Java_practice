eb35a7f229f94142f90b9dd8c6242e2348e9c98c
yowaikami <hinzyakushi@gmail.com>
2020-05-15 00:50:23 +0900

    クラスを活用したプログラム完

--- JavaWcp/src/text_practice/lesson03.java
+++ JavaWcp/src/text_practice/lesson03.java
@@ -1,5 +1,14 @@
 package text_practice;
 
-public class lesson03 {
+import java.io.Console;
 
+public class lesson03 {
+	public static void main(String[] args) {
+		double number;
+		Console console = System.console();
+		System.out.println("平方根を求めます");
+		System.out.println("1~10000までの任意の数値を入力してください");
+		number = Double.parseDouble(console.readLine());
+		System.out.println(number + "の平方根は" + Math.sqrt(number) + "です");
+	}
 }
