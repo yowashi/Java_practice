21f3a69725c1fca90bab4234fa535c64ba3fbe15
yowaikami <hinzyakushi@gmail.com>
2020-06-09 21:55:12 +0900

    状況に応じたメッセージを出す

--- JavaWcp/src/text_practice/lesson05.java
+++ JavaWcp/src/text_practice/lesson05.java
@@ -4,7 +4,8 @@ import java.io.Console;
 
 public class lesson05 {
 	public static void main(String[] args) {
-		final int answer = 3;
+		double dblAnswer = Math.random()*10 + 1;
+		int  intAnswer = (int)dblAnswer;
 		int input;
 		System.out.println("コンピュータが思い浮かべた数字を当ててね");
 		System.out.println("[これだ]と思うすうじを1~10まで入力しましょう");
@@ -12,10 +13,12 @@ public class lesson05 {
 		Console console = System.console();
 		input = Integer.parseInt(console.readLine());
 
-		if (input == answer) {
+		if (input == intAnswer) {
 			System.out.println("お見事、アタリです");
+		} else if (input < intAnswer) {
+			System.out.println("残念、小さすぎました。答えは" + intAnswer + "です");
 		} else {
-			System.out.println("残念、ハズレです");
+			System.out.println("残念、大きすぎました。答えは" + intAnswer + "です");
 		}
 	}
 }
