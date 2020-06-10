c2fff47e7b60f4ed43ec816ecf48c2955f5a8820
yowaikami <hinzyakushi@gmail.com>
2020-06-10 15:28:45 +0900

    配列!

---
+++ JavaWcp/src/text_practice/lesson08.java
@@ -0,0 +1,18 @@
+package text_practice;
+
+public class lesson08 {
+	public static void main(String[] args) {
+		String[] list = new String[5];
+		list[0] = "Taro yamada";
+		list[1] = "Oka yamada";
+		list[2] = "Kaoru yamada";
+		list[3] = "Sato yamada";
+		list[4] = "Tomori yamada";
+
+		System.out.println(list[0]);
+		System.out.println(list[1]);
+		System.out.println(list[2]);
+		System.out.println(list[3]);
+		System.out.println(list[4]);
+	}
+}
