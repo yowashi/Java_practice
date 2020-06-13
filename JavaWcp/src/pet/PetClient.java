db9832b711227f4caf28dd986750bc0043b822b9
yowaikami <hinzyakushi@gmail.com>
2020-06-13 11:40:24 +0900

    継承を用いて会話機能の追加

--- JavaWcp/src/pet/PetClient.java
+++ JavaWcp/src/pet/PetClient.java
@@ -1,19 +1,37 @@
 package pet;
 
-class Pet {
-	private String type;
-	private String name;
-	private final String message = "はじめまして";
+import java.io.Console;
 
-	Pet(String type, String name){
-		this.setType(type);
-		this.setName(name);
+//FriendlyPetのクラス
+class FriendlyPet extends Pet {
+	FriendlyPet(String type, String name){
+		super(type, name);
 	}
+	void greet() {
+		super.greet();
+		System.out.println("何か話しかけてみて");
+		talk();
+	}
+	void talk() {
+		Console console = System.console();
+		String words = console.readLine();
+		System.out.println(words + "っ言った?");
+		System.out.println("おもしろいこというね");
+	}
+}
+	class Pet{
+		protected String type;
+		protected String name;
+		protected static String message = "はじめまして";
 
+		Pet(String type, String name){
+			this.setType(type);
+			this.setName(name);
+		}
 	void greet() {
 		System.out.println(message);
-		System.out.println("僕は" + this.getType() + "の" + this.getName() + "です");
-	}
+		System.out.println("僕は" + this.getType() + "の" + this.getName() +"です");
+		}
 	/*Setter & Getter*/
 	String getType() {
 		return this.type;
@@ -27,13 +45,12 @@ class Pet {
 	void setName(String name) {
 		this.name = name;
 	}
-
 }
 
 /*petClientクラス(petクラスを呼び出して利用するクラス)*/
 public class PetClient{
 	public static void main(String[] args) {
-		Pet pet = new Pet("犬","ぽち");
+		FriendlyPet pet = new FriendlyPet("犬","ぽち");
 		pet.greet();
 	}
 }
